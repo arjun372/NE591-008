3dabfc270037c661a234c4cfdce74bb77f476ef0
Arjun Earthperson <aarjun@ncsu.edu>
2023-10-11 23:59:15 -0400

    WIP

--- src/utils/Profiler.h
+++ src/utils/Profiler.h
@@ -1,8 +1,8 @@
 /**
  * @file Profiler.h
  * @author Arjun Earthperson
- * @date 08/30/2023
- * @brief This file contains helper classes and functions for profiling.
+ * @date 10/11/2023
+ * @brief TODO:: DOCUMENT
  */
 
 #ifndef NE591_008_PROFILER_H
@@ -15,81 +15,127 @@
 #include <random>
 #include <utility>
 
+#include "CheckBounds.h"
 #include "Stopwatch.h"
+#include "json.hpp"
+#include "math/blas/Stats.h"
 
 // TODO:: DOCUMENT
-struct ProfilerOptions {
-    size_t runs;
-    size_t timeout;
-};
-
-/**
- * @brief A class that provides methods for profiling the execution time of functions.
- */
+template <typename FunctionType>
 class Profiler {
   public:
 
-    explicit Profiler(ProfilerOptions opts) {
-        options = std::move(opts);
-    }
-
-    explicit Profiler() {
-        options.runs = 1;
-        options.timeout = 0;
-    }
-
-    explicit Profiler(size_t runs) {
-        options.runs = runs;
+    // TODO:: DOCUMENT
+    explicit Profiler(FunctionType function, size_t runs = 1, long double timeout = 0, std::string description = ""): _function(function) {
+        _timedOut = false;
+        _totalRuns = runs > 0 ? runs : 1;
+        _timeout = timeout > 0 ? timeout : 0;
+        _stopwatches = std::vector<Stopwatch<Nanoseconds>>(0);
+        _description = description;
+        _summary = MyBLAS::Stats::Summary<long double>();
     }
 
     // TODO:: DOCUMENT
-    size_t tick() {
-        return 1;
+    Profiler &run() {
+        resetRuns();
+        if (_timeout > 0) {
+            _timedOut = runWithTimeout();
+        } else {
+            runNoTimeout();
+            _timedOut = false;
+        }
+        summarize(_summary, _stopwatches);
+        return *this;
     }
 
-    // t
-    size_t tock() {
-        return 0;
+    // TODO:: DOCUMENT
+    friend std::ostream &operator<<(std::ostream &os, const Profiler &m) {
+        os << R"(::::::::::::::::::::::::::::::: PROFILE SUMMARY ::::::::::::::::::::::::::::::::)"<<std::endl;
+        os << "["<<m._stopwatches.size()<<"/"<<m._totalRuns<<"] : "<<m._description<<std::endl;
+        os << R"(::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::)"<<std::endl;
+        os << m._summary << std::endl;
+        os << R"(::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::)"<<std::endl;
+        return os;
     }
+
+    // TODO:: DOCUMENT
+    const MyBLAS::Stats::Summary<long double> &getSummary() const { return _summary; }
 
   private:
-    ProfilerOptions options;
-    //Stopwatch<Nanoseconds> stopwatch; ///< A Stopwatch object for measuring time in microseconds.
-
-
-
-  public:
+    // TODO:: DOCUMENT
+    FunctionType _function;
 
     // TODO:: DOCUMENT
-    static boost::program_options::options_description buildCommandlineOptions() {
-        boost::program_options::options_description profiler("Benchmarks");
-        profiler.add_options()
-                ("bench-runs,R", boost::program_options::value<long double>()->default_value(1), "= <R> runs to perform")
-                    ("bench-timeout,T", boost::program_options::value<long double>()->default_value(0), "= Timeout after <T> seconds [0=never]")
-                        ("bench-output,O", boost::program_options::value<std::string>(), "= path for the benchmark output JSON");
-        return profiler;
+    size_t _totalRuns;
+
+    // TODO:: DOCUMENT
+    long double _timeout;
+
+    // TODO:: DOCUMENT
+    bool _timedOut;
+
+    std::string _description;
+
+    // TODO:: DOCUMENT
+    std::vector<Stopwatch<Nanoseconds>> _stopwatches;
+
+    MyBLAS::Stats::Summary<long double> _summary;
+
+    // TODO:: DOCUMENT
+    void runNoTimeout() {
+        for(size_t i = 0; i < _totalRuns; i++) {
+            auto stopwatch = Stopwatch<Nanoseconds>().restart();
+            _function();
+            stopwatch.click();
+            _stopwatches.emplace_back(stopwatch);
+        }
     }
 
     // TODO:: DOCUMENT
-    // validate the input options, prompting the user if they need to be fixed
-    static void validateOptions(boost::program_options::variables_map &map) {
+    bool runWithTimeout() {
+        auto timeoutWatch = Stopwatch<Nanoseconds>();
+        timeoutWatch.restart();
 
+        for(size_t i = 0; i < _totalRuns; i++) {
+            // timed out
+            if(timeoutWatch.peek_elapsed_time().count() > _timeout) {
+                _timedOut = true;
+                break;
+            }
+            // run the calculation
+            auto stopwatch = Stopwatch<Nanoseconds>().restart();
+            _function();
+            stopwatch.click();
+            _stopwatches.emplace_back(stopwatch);
+        }
+
+        return _timedOut;
     }
 
     // TODO:: DOCUMENT
-    static ProfilerOptions Options(boost::program_options::variables_map &map) {
-        validateOptions(map);
-        ProfilerOptions options = {
-            .runs = static_cast<size_t>(map["bench-runs"].as<long double>()),
-            .timeout = static_cast<size_t>(map["bench-timeout"].as<long double>()),
-            //.ostream = std::ofstream(map["bench-output"].as<std::string>()),
-        };`11
-        return options;
+    void resetRuns() {
+        _timedOut = false;
+        _stopwatches.clear();
+        _stopwatches.resize(0);
+        _summary = MyBLAS::Stats::Summary<long double>();
     }
 
     // TODO:: DOCUMENT
-    static Profiler Init(boost::program_options::variables_map &map) {
-        return Profiler(Profiler::Options(map));
+    static void summarize(MyBLAS::Stats::Summary<long double> &summary, const std::vector<Stopwatch<Nanoseconds>> &stopwatches) {
+        // Create an array of durations
+        MyBLAS::Vector<long double> durations(stopwatches.size());
+        std::transform(stopwatches.begin(), stopwatches.end(), durations.getData().begin(), [](const Stopwatch<Nanoseconds>& stopwatch) { return stopwatch.duration().count(); });
+
+        // Calculate statistics
+        summary.min = MyBLAS::Stats::min(durations);
+        summary.max = MyBLAS::Stats::max(durations);
+        summary.sum = MyBLAS::Stats::sum(durations);
+        summary.mean = MyBLAS::Stats::mean(durations);
+        summary.variance = MyBLAS::Stats::variance(durations);
+        summary.stddev = MyBLAS::Stats::std(durations);
+        summary.median = MyBLAS::Stats::median(durations);
+        summary.p5th = MyBLAS::Stats::percentile(durations, 5);
+        summary.P95th = MyBLAS::Stats::percentile(durations, 95);
     }
 };
 
